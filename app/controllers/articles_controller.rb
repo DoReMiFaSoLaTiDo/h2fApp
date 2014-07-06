020ddf2f4367f461a2492e90fe23e45ae4722f96
Administrator <mike@ubuntu.(none)>
2014-07-05 19:39:19 -0500

    Form-based workflow completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
 	def index
 		@articles = Article.all
 	end
@@ -6,4 +7,40 @@ class ArticlesController < ApplicationController
 	def show
 		@article = Article.find(params[:id])
 	end
+
+	def new
+		@article = Article.new
+	end
+
+#	def create
+#		@article = Article.new
+#		@article.title = params[:article][:title]
+#		@article.body = params[:article][:body]
+#		@article.save
+#		redirect_to article_path(@article)
+#	end
+	
+	def create
+	  @article = Article.new(article_params)
+#	  @article = Article.new(params[:article])
+	  @article.save
+	  flash.notice = "New Article '#{@article.title}' Created!"
+	  redirect_to article_path(@article)
+	end
+  
+  def destroy
+    @article = Article.find(params[:id])
+    @article.delete
+    flash.notice = "Article '#{@article.title}' Deleted!"
+#    @articles = Article.all
+    redirect_to articles_path(@articles)
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    flash.notice = "Article '#{@article.title}' Updated!"
+    redirect_to article_path(@article)
+  end
+
 end
